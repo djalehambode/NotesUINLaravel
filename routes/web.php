433e670b09297532f2bd3d8ac775feb8c1351a38
h <himaachanamoussa@gmail.com>
2024-07-11 00:00:15 +0100

    2em commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('connexion');
 });
+Route::get('/classes', [ClasseController::class, 'index'])->name('classes.index');
+Route::get('/classes/{classe}/modules', [ModuleController::class, 'index'])->name('modules.index');
+Route::get('/classes/{classe}/etudiants', [EtudiantController::class, 'index'])->name('etudiants.index');
+Route::get('/modules/{module}/notes', [NoteController::class, 'index'])->name('notes.index');
+Route::post('/modules/{module}/notes', [NoteController::class, 'store'])->name('notes.store');
